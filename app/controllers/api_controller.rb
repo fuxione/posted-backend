983828bebbea124884118afe892fe113e1ac81c5
Travis <travis@marketcake.com>
2025-07-05 04:53:51 -0700

    format

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,17 @@
 class ApiController < ActionController::API
   def success(data = nil)
-    payload = { status: "success", data: data, meta: { seq: params[:seq] || 0 } }
+    payload = {
+      status: "success",
+      data: data,
+      meta: { seq: params[:seq] || 0 },
+    }
     render json: payload
   end
 
   def error(messages, code = 422)
-    render json: { status: "error", errors: messages }, status: code
+    render json: {
+      status: "error",
+      errors: messages,
+    }, status: code
   end
 end
